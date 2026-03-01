52e8f39d21bb4bd47f9cc594b1174210fbe128d1
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-01 17:09:38 +0700

    Update the user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-//@route     GET api/users
-//@desc      test route
+//@route     post api/users
+//@desc      register user
 //@access    Public
-router.get('/', (req, res) => res.send('Users route'));
+router.post('/', (req, res) => {
+	console.log(req.body);
+
+	res.send('Users route');
+});
 
 module.exports = router;
